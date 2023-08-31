93a678eb5bf13675db6763dbfadc25e63789a613
Sandino López <uri.sandi.maxi@gmail.com>
2023-08-30 22:48:28 -0300

    creadas primeras migrations

---
+++ database/migrations/2023_08_27_053521_usuarios.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Usuarios extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('usuarios', function (Blueprint $table){
+            $table->id('id_usuario');
+            $table->string('username', 32)->unique();
+            $table->string('password', 256);
+            $table->string('nombre', 32);
+            $table->string('apellido', 32);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('usuarios');
+    }
+}
