c4fbabe7b9a23b79c528a9b15d073d301e58c673
Manuel Rodriguez <manu.ro008@gmail.com>
2023-09-05 21:40:00 -0300

    se hizo la tabla gestiona_fin

---
+++ database/migrations/2023_09_06_003227_gestiona_fin.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class GestionaFin extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('gestiona', function (Blueprint $table){
+            $table->foreign('id')->references('id')->on('gestiona');
+            $table->timestamp('fecha_fin');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('gestiona_fin');
+    }
+}
