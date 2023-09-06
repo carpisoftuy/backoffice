fddbd44755f16167aca22dd4f104da12495bdb92
Manuel Rodriguez <manu.ro008@gmail.com>
2023-09-05 23:21:53 -0300

    se creo tabla paquete_contiene_bulto

---
+++ database/migrations/2023_09_06_021511_almacen_contiene_bulto.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AlmacenContieneBulto extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('almacen_contiene_bulto', function (Blueprint $table){
+            $table->id();
+            $table->unsignedBigInteger('id_bulto');
+            $table->foreign('id_bulto')->references('id')->on('bulto')->unique('bulto_fecha');
+            $table->timestamp('fecha_inicio')->unique('bulto_fecha');
+            $table->unsignedBigInteger('id_almacen');
+            $table->foreign('id_almacen')->references('id')->on('almacen');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('paquete_contiene_bulto');
+    }
+}
