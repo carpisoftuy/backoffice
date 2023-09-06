8b4ddf7dd5df66e72c57ec011864ba0eb269c751
Manuel Rodriguez <manu.ro008@gmail.com>
2023-09-05 23:32:21 -0300

    se creo tabla almacen_contiene_paquete

--- database/migrations/2023_09_06_022829_almacen_contiene_paquete.php
+++ database/migrations/2023_09_06_022829_almacen_contiene_paquete.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AlmacenContieneBulto extends Migration
+class AlmacenContienePaquete extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,11 @@ class AlmacenContieneBulto extends Migration
      */
     public function up()
     {
-        Schema::create('almacen_contiene_bulto', function (Blueprint $table){
+        Schema::create('almacen_contiene_paquete', function (Blueprint $table){
             $table->id();
-            $table->unsignedBigInteger('id_bulto');
-            $table->foreign('id_bulto')->references('id')->on('bulto')->unique('bulto_fecha');
-            $table->timestamp('fecha_inicio')->unique('bulto_fecha');
+            $table->unsignedBigInteger('id_paquete');
+            $table->foreign('id_paquete')->references('id')->on('paquete')->unique('paquete_fecha');
+            $table->timestamp('fecha_inicio')->unique('paquete_fecha');
             $table->unsignedBigInteger('id_almacen');
             $table->foreign('id_almacen')->references('id')->on('almacen');
         });
@@ -30,6 +30,6 @@ class AlmacenContieneBulto extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('almacen_contiene_bulto');
+        Schema::dropIfExists('almacen_contiene_paquete');
     }
 }
