4e96744027d65d3ee1674889bee3abff957d672c
sosam1 <sosam1763@gmail.com>
2023-11-02 18:49:25 -0300

    test get vehiculo

---
+++ tests/Feature/GetVehiculoTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class GetVehiculoTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testGetVehiculos() {
+        $response = $this->get('/v2/vehiculos');
+        $response->assertStatus(200);
+    } 
+}
