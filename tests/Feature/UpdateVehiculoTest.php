522f2d72505d844460b17d0a8f2bc85b9a5cb303
sosam1 <sosam1763@gmail.com>
2023-11-02 18:58:44 -0300

    test update vehiculo

---
+++ tests/Feature/UpdateVehiculoTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Models\Vehiculo;
+class UpdateVehiculoTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testUpdateVehiculo(){
+
+        $vehiculo = Vehiculo::create([
+            'codigo_pais' => "UYU",
+            'matricula' => "SBZ-1441",
+            'capacidad_volumen' => 1000,
+            'capacidad_peso' => 1000,
+            'peso_ocupado' => 0,
+            'volumen_ocupado' => 0
+        ]);
+
+        $nuevoCodigo = "BO";
+
+        $response = $this->put("/v2/vehiculos/{$vehiculo->id}", [
+            'codigo_pais' => $nuevoCodigo,
+            'matricula' => "SBZ-1441",
+            'capacidad_volumen' => 1000,
+            'capacidad_peso' => 1000,
+            'peso_ocupado' => 0,
+            'volumen_ocupado' => 0
+        ]);
+
+        $response->assertStatus(200);
+
+        // Verifica que los datos del vehiculo actualizado coincidan con los nuevos datos
+        $this->assertEquals($nuevoCodigo, $vehiculo->fresh()->codigo_pais);
+
+        $vehiculo->delete();
+
+    }
+}
