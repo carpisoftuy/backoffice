38ef0c13aa3519266da0821d92fa351f514bec67
sosam1 <sosam1763@gmail.com>
2023-11-06 09:34:08 -0300

    navegabilidad

--- resources/views/vehiculos.blade.php
+++ resources/views/vehiculos.blade.php
@@ -23,6 +23,7 @@
         <ul class="nav-list">
             <li class="list-element-logo"><a href="./index.html"><img class="logo" src="{{ asset('img/carpifast.svg') }}"></a></li>
             <li class="list-element"><a id="inicio" href="./index.html">Inicio</a></li>
+            <li class="list-element"><a id="inicio" href="./almacenes">Almacenes</a></li>
             <li class="list-element"><a id="inicio" href="./vehiculos">Vehiculos</a></li>
             <li class="list-element"><a id="panel" href="../">Panel backoffice</a></li>
             <li class="list-element"><a id="registro" href="../usuarios/crear">Registrar usuario</a></li>
